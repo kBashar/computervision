1fde96147b4c5299688a87e578599e4ebcd2636c
kBashar <khyrulbashar.0.1@gmail.com>
2025-10-30 21:50:20 +0600

    refactor for better file structure

---
+++ main.py
@@ -0,0 +1,5 @@
+from src.day_1.delete_channel import delete_channel
+
+
+if __name__=="__main__":
+    delete_channel()
---
+++ src/day_1/delete_channel.py
@@ -0,0 +1,35 @@
+
+import cv2 as cv
+import sys
+import numpy as np
+
+from src.utils import show_image
+
+# def show_image(title: str, img):
+#     cv.imshow(title, img)
+
+#     # Wait for key press and ensure clean exit
+#     while True:
+#         if cv.waitKey(1) & 0xFF == 27:  # ESC key
+#             break
+#         if cv.getWindowProperty(title, cv.WND_PROP_VISIBLE) < 1:
+#             break
+
+#     cv.destroyAllWindows()
+#     cv.waitKey(1)  # Extra waitKey to ensure window closes
+
+def delete_channel():
+    img = cv.imread("images/Instagram_logo_2022.webp", cv.IMREAD_UNCHANGED)
+
+    if img is None:
+        sys.exit("Could not read the image.")
+
+    print(f"Dimension of the image: {img.shape}")
+
+    print(f"Data type: {img.dtype}")
+
+    #img[:, :, 0] = np.zeros([img.shape[0], img.shape[1]])
+
+    img[:, :, 2] = 0
+
+    show_image("Channel Removed Image", img)
